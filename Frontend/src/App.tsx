59eb47bab851b89f74626fdd7f903d4aabbad764
Mitali-Patle <mitalipatle2@gmail.com>
2025-10-11 08:44:07 +0530

    Task module updated with Visualization

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SkillExchange from './pages/LandingPage';
 import Chat from './pages/Chat';
 import TaskList from "./components/TaskList";
+import TaskStatsChart from "./components/TaskStatsChart";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<SkillExchange />} />
           <Route path="/chat" element={<Chat />} />
           <Route path="/tasks" element={<TaskList />} />
+          <Route path="/task-stats" element={<TaskStatsChart />} />
         </Routes>
       </>
     </Router>
